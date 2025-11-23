e5efc3a0a9cace7cecc119c0626e34dce840977d
shimaaadel0448-123 <shimaaadel0448@gmail.com>
2025-11-23 14:01:26 +0200

    حل مشكلة budgets وعمل build بدون Router

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Header } from './Components/header/header';
 import { Main } from './Components/main/main';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Main],
+  imports: [Main],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
