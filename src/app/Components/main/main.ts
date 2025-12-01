495926a40ce5881c7ecc5409ca0717b160a5537e
shimaaadel0448-123 <shimaaadel0448@gmail.com>
2025-12-01 21:16:20 +0200

    تحديث الواجهة الرئيسية + responsive + sidebar متحرك في الموبايل

--- src/app/Components/main/main.ts
+++ src/app/Components/main/main.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { Header } from '../header/header';
-import { SideBar } from '../side-bar/side-bar';
-import { Content } from '../content/content';
+
 
 @Component({
   selector: 'app-main',
-  imports: [Header,SideBar,Content],
+  imports: [],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
 export class Main {
+  sidebarOpen = false;
 
 }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component, signal } from '@angular/core';
-import { Header } from './Components/header/header';
 import { Main } from './Components/main/main';
 @Component({
   selector: 'app-root',
